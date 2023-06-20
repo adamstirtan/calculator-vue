1660e3d719dde64bba68ea87ce8600515311ed62
Adam Stirtan <adamstirtan@gmail.com>
2023-06-19 22:19:53 -0400

    Adds buttons programmatically

---
+++ src/buttons.js
@@ -0,0 +1,82 @@
+export const buttons = [
+    {
+        label: '%',
+        value: '%',
+        type: 'func'
+    },
+    {
+        label: 'π',
+        value: 'Math.pi',
+        type: 'func'
+    },
+    {
+        label: '',
+        value: '',
+        type: 'empty'
+    },
+    {
+        label: 'C',
+        value: 'ESC',
+        type: 'clear'
+    },
+    {
+        label: '7',
+        value: '7',
+        type: 'number'
+    },
+    {
+        label: '8',
+        value: '8',
+        type: 'number'
+    },
+    {
+        label: '9',
+        value: '9',
+        type: 'number'
+    },
+    {
+        label: '÷',
+        value: '/',
+        type: 'operator'
+    },
+    {
+        label: '4',
+        value: '4',
+        type: 'number'
+    },
+    {
+        label: '5',
+        value: '5',
+        type: 'number'
+    },
+    {
+        label: '6',
+        value: '6',
+        type: 'number'
+    },
+    {
+        label: 'x',
+        value: '*',
+        type: 'operator'
+    },
+    {
+        label: '1',
+        value: '1',
+        type: 'number'
+    },
+    {
+        label: '2',
+        value: '2',
+        type: 'number'
+    },
+    {
+        label: '3',
+        value: '3',
+        type: 'number'
+    },
+    {
+        label: '=',
+        value: '',
+        type: 'calculate'
+    }
+]
---
+++ src/components/Calculator.vue
@@ -0,0 +1,67 @@
+<script setup>
+    import { buttons } from '../buttons'
+
+    //const buttons = ref(buttons);
+
+    buttons.forEach(button => {
+        console.log(button.label)
+    })
+</script>
+
+<template>
+    <div class="container">
+        <div class="display">
+            Display
+        </div>
+        <div class="buttons">
+            <button
+                v-for="button in buttons"
+                :key="button.label"
+                :value="button.value"
+                :class="button.type"
+                @click="handleButtonClick">
+                {{ button.label }}
+            </button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .container {
+        width: 500px;
+        height: 600px;
+        border: 3px solid #000;
+        border-radius: 8px
+    }
+
+    .display {
+        height: 120px;
+        background-color: hsl(0, 0%, 0%);
+        color: #fff;
+        text-align: right;
+    }
+
+    .buttons {
+        display: grid;
+        background-color: #ededed;
+        grid-template-columns: repeat(4, 1fr);
+        grid-gap: 1rem;
+        padding: 1rem
+    }
+
+    button {
+        height: 100px;
+        border: none;
+        border-radius: 40px;
+        font-size: 2em;
+    }
+
+    button.empty {
+        visibility: hidden;
+    }
+
+    button.clear {
+        color: #fff;
+        background-color: red;
+    }
+</style>
